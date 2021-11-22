31ee2d5247a122e54cce0f24b7f2524c02644d28
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2021-11-21 21:56:42 -0300

    Update

--- photo-organizer.py
+++ photo-organizer.py
@@ -1,16 +1,20 @@
 import os
 from datetime import datetime
+from types import NoneType
 from PIL import Image
 
 def photo_shooting_date(file):
     photo = Image.open(file)
     info = photo._getexif()
-    if 36867 in info:
-        date = info[36867]
-        date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
-    else:
+    if info:
+        if 36867 in info:
+            date = info[36867]
+            date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
+        else:
+            date = datetime.fromtimestamp(os.path.getatime(file))
+    if info is None:
         date = datetime.fromtimestamp(os.path.getatime(file))
-    return info
+    return date
 
-print(photo_shooting_date('exemplo.jpg'))
+print(photo_shooting_date('vovo.jpg'))
 
