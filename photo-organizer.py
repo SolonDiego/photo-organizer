c946e86a37e56e36fecbd11f072067d2e18bfb2d
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2021-11-22 22:11:38 -0300

    update

--- photo-organizer.py
+++ photo-organizer.py
@@ -3,32 +3,44 @@ import shutil
 from datetime import datetime
 from PIL import Image
 
-def folder_path_from_date(file):
-    date = photo_shooting_date(file)
-    return date.strftime('%Y') + '/' + date.strftime('%Y-%m')
 
-def photo_shooting_date(file):
-    photo = Image.open(file)
-    info = photo._getexif()
-    if info:
-        if 36867 in info:
-            date = info[36867]
-            date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
-        else:
+class PhotoOrganizer:
+
+    extensions = ['jpg', 'jpeg', 'png' 'JPG', 'JPEG', 'PNG']
+
+
+    def folder_path_from_date(self, file):
+        date = self.photo_shooting_date(file)
+        return date.strftime('%Y') + '/' + date.strftime('%Y-%m')
+
+
+    def photo_shooting_date(self, file):
+        photo = Image.open(file)
+        info = photo._getexif()
+        if info:
+            if 36867 in info:
+                date = info[36867]
+                date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
+            else:
+                date = datetime.fromtimestamp(os.path.getatime(file))
+        if info is None:
             date = datetime.fromtimestamp(os.path.getatime(file))
-    if info is None:
-        date = datetime.fromtimestamp(os.path.getatime(file))
-    return date
+        return date
 
-def move_photo(file):
-    new_folder = folder_path_from_date(file)
-    if not os.path.exists(new_folder):
-        os.makedirs(new_folder)
-    shutil.move(file, new_folder + '/' + file)
 
-print(photo_shooting_date('vovo.jpg'))
+    def move_photo(self, file):
+        new_folder = self.folder_path_from_date(file)
+        if not os.path.exists(new_folder):
+            os.makedirs(new_folder)
+        shutil.move(file, new_folder + '/' + file)
 
-print(folder_path_from_date('vovo.jpg'))
 
-print(move_photo('vovo.jpg'))
+    def organize(self):
+        photos = [
+            filename for filename in os.listdir('.') if any(filename.endswith(ext) for ext in self.extensions)
+        ]
+        for filename in photos:
+            self.move_photo(filename)  
 
+PO = PhotoOrganizer()
+PO.organize()
