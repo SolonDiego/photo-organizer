cde8af454e87b16fc630894db9a272b76b99a5fc
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2021-11-22 21:36:53 -0300

    Update

--- photo-organizer.py
+++ photo-organizer.py
@@ -1,7 +1,11 @@
 import os
+import shutil
 from datetime import datetime
-from types import NoneType
 from PIL import Image
+
+def folder_path_from_date(file):
+    date = photo_shooting_date(file)
+    return date.strftime('%Y') + '/' + date.strftime('%Y-%m')
 
 def photo_shooting_date(file):
     photo = Image.open(file)
@@ -16,5 +20,15 @@ def photo_shooting_date(file):
         date = datetime.fromtimestamp(os.path.getatime(file))
     return date
 
+def move_photo(file):
+    new_folder = folder_path_from_date(file)
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+    shutil.move(file, new_folder + '/' + file)
+
 print(photo_shooting_date('vovo.jpg'))
+
+print(folder_path_from_date('vovo.jpg'))
+
+print(move_photo('vovo.jpg'))
 
